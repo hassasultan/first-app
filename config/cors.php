58cd2062ba5b2bc6b46107364439dce9be391fe0
Hassan Sultan <h.hsultan277@gmail.com>
2021-12-15 18:28:46 +0500

    new Commit

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,7 @@ return [
     | Here you may configure your settings for cross-origin resource sharing
     | or "CORS". This determines what cross-origin operations may execute
     | in web browsers. You are free to adjust these settings as needed.
+    new
     |
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
